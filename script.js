958880c9ad7fa6e066ac61450f2920e61af6d877
Matthieu Merlen <kiwix.shell@gmail.com>
2025-10-15 09:44:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const images = ["moto1.jpg", "moto2.jpg", "moto3.jpg",];
+let index = 0;
+const photo = document.getElementById("photo");
+
+function changeImage(){
+    index = (index + 1) % images.length;
+    photo.style.opacity = 0;
+
+    setTimeout(() => {
+        photo.src = images[index];
+        photo.style.opacity = 1;
+    }, 500);
+}
+
+setInterval(changeImage, 2000);
